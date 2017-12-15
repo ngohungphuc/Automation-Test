dee126c0323b92a380aeda8d3948a4a8ff43acb0
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-15 17:23:35 +0700

    Specflow Bindings

---
+++ BDD with Specflow/SpecflowIntro/SpecflowIntro/TestFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowIntro
+{
+    [Binding]
+    public sealed class TestFixture
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        [BeforeFeature]
+        public static void CallFirstInFeature()
+        {
+            Console.WriteLine("Calling before Feature");
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            Console.WriteLine("Calling before Scenario");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Console.WriteLine("Calling after Scenario");
+        }
+    }
+}
