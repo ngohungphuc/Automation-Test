77af9c07ae839fa44c9ef2e9bf1b879948fadd3d
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-19 15:24:14 +0700

    Scoped Binding

---
+++ BDD with Specflow/SpecflowBinding/SpecflowBinding/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowBinding
+{
+    [Binding]
+    public sealed class Login
+    {
+        [Given(@"I have opened the application")]
+        public void GivenIHaveOpenedTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I login to application")]
+        //restrict in the specific feature
+        [Scope(Feature ="User_Login")]
+        public void GivenILoginToApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I see user portal")]
+        public void ThenISeeUserPortal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I see customer portal")]
+        public void ThenISeeCustomerPortal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ BDD with Specflow/SpecflowBinding/SpecflowBinding/LoginFeature.feature
@@ -0,0 +1,8 @@
+﻿Feature: Customer_Login
+	Simple feature to demonstrate customer login feature
+
+@customer
+Scenario: Login for customer portal
+	Given I have opened the application
+	And I login to application
+	Then I see customer portal
